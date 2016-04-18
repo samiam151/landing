9b2f1b13c4fa547b469a0150310abd3236eaafb3
samiam151 <samiam151@gmail.com>
2016-04-18 17:27:31 -0400

    Added picture and gradient to main page

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 $('section').each(function(){
    var winHeight = $(window).height();
-   $(this).css('height', (winHeight + (winHeight * 0.20)));
+   $(this).css('height', (winHeight + (winHeight * 0.1)));
    $(this).css('display', 'absolute');
 });
-
 
 var app = angular.module('app', ['ui.router']);
 
@@ -18,20 +17,25 @@ app.service('experiences', function(){
    this.works = [
       {
          header: 'Interactive Bubble Chart',
+         brief: 'Data Visualization',
          skills: 'HTML, SCSS, JQUERY, D3, PYTHON',
-         url: 'http://forgenerationstocome.org/#/bubble-chart'
+         url: 'http://forgenerationstocome.org/#/bubble-chart',
+         image: '../assets/screenshot_of_bubble_graph.png'
       },
       {
          header: 'Interactive Piano',
+         brief: 'Web Audio API Experiment',
          skills: 'HTML, SCSS, JAVASCRIPT, WEB AUDIO API',
-         url: 'http://nick-sam.com/interactivePiano/'
+         url: 'http://nick-sam.com/interactivePiano/',
+         image: '../assets/screenshot_of_piano.png'
       },
       {
          header: 'Nova Energy',
+         brief: 'Web Design from Mock-Up',
          skills: 'HTML, SCSS, BOOTSTRAP',
-         url: 'http://nick-sam.com/aqua/'
+         url: 'http://nick-sam.com/aqua/',
+         image: '../assets/screenshot_of_nova_energy.png'
       }
    ];
-
 })
 ;
